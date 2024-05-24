852db52e6f0c9031d3114dc485a1d0e53dbce50f
Everesh <everesh@proton.me>
2024-05-25 00:01:26 +0200

    Implement hash method

--- lib/hash_map.rb
+++ lib/hash_map.rb
@@ -10,4 +10,10 @@ class HashMap
   private
 
   attr_accessor :capacity, :load, :data
+
+  def hash(key)
+    hash_code = 0
+    key.each_char { |char| hash_code = (PRIME * hash_code) + char.ord }
+    hash_code
+  end
 end
